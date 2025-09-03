64c06d55dfaff618becc1a242f66753b99709c3e
danilotec <danilo0123499@gmail.com>
2025-09-03 14:48:54 -0300

    configurando whitenoise

--- backend/monitoramento/settings.py
+++ backend/monitoramento/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_lzwcqo0@$*3ielz-mktmd6lq7jhfa5cd%8s*3&qtfo=&w4_h_'
+SECRET_KEY = os.getenv('DJANGO_KEY','django-insecure-_lzwcqo0@$*3ielz-mktmd6lq7jhfa5cd%8s*3&qtfo=&w4_h_' )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -43,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -50,7 +54,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_AUTOREFRESH = True
 ROOT_URLCONF = 'monitoramento.urls'
 
 TEMPLATES = [
@@ -117,7 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- dockerfile
+++ dockerfile
@@ -10,4 +10,4 @@ COPY . /app/
 
 EXPOSE 8000
 
-CMD ["sh", "-c", "python backend/manage.py migrate && python backend/manage.py runserver 0.0.0.0:8000"]
+CMD ["sh", "-c", "python backend/manage.py migrate && python backend/manage.py && python manage.py collectstatic --noinput && gunicorn --bind 0.0.0.0:8000 --workers 3 --timeout 120 monitoramento.wsgi:application"]
