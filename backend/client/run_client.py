e963a942e4e295c2dbdefb551f71568d51a0568b
Danilotec <danilo0123499@gmail.com>
2025-09-27 13:42:47 -0300

    mudanças para produção

--- backend/client/run_client.py
+++ backend/client/run_client.py
@@ -11,14 +11,14 @@ def run_mqtt():
     user = str(os.getenv('USER_BROKER'))
     password = str(os.getenv('PASSWORD_BROKER'))
     topics = str(os.getenv('TOPICS'))
-   
-    redis_host = 'localhost'
+    
+    redis_host = os.getenv('REDIS_HOST')
     redis_port = 6379
     redis_db = 0
-    redis_password = None
-    
+    redis_password = os.getenv('REDIS_PASSWORD')
+
     client = MqttHandler(broker, port, user, password, topics)
-    client.set_redis_connection(host=redis_host,
+    client.set_redis_connection(host=redis_host, #type:ignore
                                 port=redis_port,
                                 db=redis_db,
                                 password=redis_password)
